d32f46198db954260ebb6b761c5720c6ba168bb3
KILATIV100 <49151005+KILATIV100@users.noreply.github.com>
2025-12-08 18:02:54 +0200

    Refactor Settings class for environment variables
    Updated the Settings class to require ADMIN_IDS and FEEDBACK_CHAT_ID, and modified DATABASE_URL to be loaded from environment variables.

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-# config.py (без змін, але DB_URL з env)
 from pydantic_settings import BaseSettings
 from typing import List
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
-    ADMIN_IDS: List[int] = [7363233852]  # твої ID, через кому в []
-    FEEDBACK_CHAT_ID: int = +AzCLSC9rTz5lOTQy  # чат для логів (заміни на реальний з групи)
-    DATABASE_URL: str # з Railway
+    DATABASE_URL: str
+    ADMIN_IDS: List[int]
+    FEEDBACK_CHAT_ID: int   # обов'язково вказуємо тип int
 
     class Config:
         env_file = ".env"
+        env_file_encoding = "utf-8"
 
 settings = Settings()
